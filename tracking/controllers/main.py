419bc9bf967cccbda83634b46648ed8cd00eb35c
Nay3li <nayeli.valencia@xmarts.com.mx>
2017-10-20 10:46:32 -0500

    Descarga de clientes

--- tracking/controllers/main.py
+++ tracking/controllers/main.py
@@ -8,7 +8,8 @@ import json
 _logger = logging.getLogger(__name__)
 lista=[]
 class trackController(http.Controller):
-    #parameter = {"CAT_CL":'2017-10-09 16:47:55',"CAT_PR":'-'}
+    #parameter = {CAT_CL=2017-10-09 16:47:55,"CAT_PR":'-'}
+    #localhost:8069/track/login?BD=odoo11&N=admin&P=admin
 
 
     @http.route('/track/login', type='http', auth="none",methods=['GET'])
@@ -34,6 +35,8 @@ class trackController(http.Controller):
         print(lista[0])
         print(lista[1])
         print(lista[2])
+        print('CAT_CL' in request.params)
+
         odoo = client.OdooClient(protocol='xmlrpc', host='localhost', dbname=lista[0], port=8069, debug=True)
         odoo.ServerInfo()
         odoo.Authenticate(lista[1], lista[2])
@@ -52,25 +55,46 @@ class trackController(http.Controller):
             valspaises = {}
             insertcliente = ''
             ####################CLIENTES#####################################
-            fecha_inicio = request.params['CAT_CL'] + '.0'
-            print("fecha inicio %s" % fecha_inicio)
-            cont = odoo.SearchCount('res.partner',
-                                    [('write_date', '>=', fecha_inicio)])
+            if 'CAT_CL' in request.params:
+                fecha_inicio = request.params['CAT_CL'] + '.0'
+                print("fecha inicio %s" % fecha_inicio)
+                cont = odoo.SearchCount('res.partner',
+                                        [('write_date', '>=', fecha_inicio)])
 
-            partners = odoo.SearchRead('res.partner',[('write_date', '>=', fecha_inicio)],
-                                        ['name', 'display_name', 'website', 'function', 'phone', 'mobile', 'email', 'active'])
-            data = str(request.params['CAT_CL'] + ' ' )+str(cont)
+                partners = odoo.SearchRead('res.partner',[('write_date', '>=', fecha_inicio)],
+                                            ['name', 'display_name', 'website', 'function', 'phone', 'mobile', 'email', 'active'])
+                data = str(request.params['CAT_CL'] + ' ' )+str(cont)
 
-            for p in partners:
-                if str(p['active']) == False:
-                    activo = 0
-                else:
-                    activo = 1
-                
-                insertcliente = insertcliente  + "INSERT OR REPLACE INTO CLIENTE(id,id_servidor,nombre,nombre_comercial,sitio_web,puesto_trabajo,tel,movil,fax,email,id_precio_lista,active) VALUES((select id from cliente where id_servidor='" + str(p['id']) + "'), '" + str(p['id']) + "','" + str(p['name']) + "','" + str(p['display_name']) + "','" + str(p['website']) + "','" + str(p['function']) + "','" + str(p['phone']) + "','" + str(p['mobile']) + "','','" + str(p['email']) + "','"+1+"','" + str(activo) + "');"
-            vals = {
-                data: insertcliente
-            }
+                for p in partners:
+                    if str(p['active']) == False:
+                        activo = 0
+                    else:
+                        activo = 1
+
+                    insertcliente = insertcliente  + "INSERT OR REPLACE INTO CLIENTE(id,id_servidor,nombre,nombre_comercial,sitio_web,puesto_trabajo,tel,movil,fax,email,id_precio_lista,active) VALUES((select id from cliente where id_servidor='" + str(p['id']) + "'), '" + str(p['id']) + "','" + str(p['name']) + "','" + str(p['display_name']) + "','" + str(p['website']) + "','" + str(p['function']) + "','" + str(p['phone']) + "','" + str(p['mobile']) + "','','" + str(p['email']) + "','"+str(1)+"','" + str(activo) + "');"
+                vals = {
+                    data: insertcliente
+                }
+
+            else:
+                cont = odoo.SearchCount('res.partner',[])
+
+                partners = odoo.SearchRead('res.partner', [],
+                                           ['name', 'display_name', 'website', 'function', 'phone', 'mobile', 'email',
+                                            'active'])
+                data = str(cont)
+
+                for p in partners:
+                    if str(p['active']) == False:
+                        activo = 0
+                    else:
+                        activo = 1
+
+                    insertcliente = insertcliente + "INSERT OR REPLACE INTO CLIENTE(id,id_servidor,nombre,nombre_comercial,sitio_web,puesto_trabajo,tel,movil,fax,email,id_precio_lista,active) VALUES((select id from cliente where id_servidor='" + str(p['id']) + "'), '" + str(p['id']) + "','" + str(p['name']) + "','" + str(p['display_name']) + "','" + str(p['website']) + "','" + str(p['function']) + "','" + str(p['phone']) + "','" + str(p['mobile']) + "','','" + str(p['email']) + "','" + str(1) + "','" + str(activo) + "');"
+                vals = {
+                    data: insertcliente
+                }
+
             json_list['clientes'].append(vals)
             json_list['clientes_dirs'].append(valscld)
             json_list['productos'].append(valsproduct)
